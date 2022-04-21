3002aad7d06d735bf94e5b3b6bce0c7a2ac0c7ee
renlingjie <309273719@qq.com>
2022-04-21 16:53:47 +0800

    # ES6.3修复查询不到数据的问题，即在@Document指定type为doc(高版本如7.9.3为_doc，不指定默认就是_doc)

--- domain/search/foodie-search-service/src/main/java/com/rlj/search/service/pojo/Items.java
+++ domain/search/foodie-search-service/src/main/java/com/rlj/search/service/pojo/Items.java
@@ -7,7 +7,8 @@ import org.springframework.data.elasticsearch.annotations.FieldType;
 
 //indexName就是我们同步使用的索引的名称--foodie-items-ik。createIndex表示没有索引的情况下是否创建一个
 //索引，默认是会创建的（类似于JPA一个类创建后加上@Table可以创建一个表），这里我们已经创建了，虽不影响，但置为false
-@Document(indexName = "foodie-items-ik",createIndex = false)
+// 大坑!!6.3.0自动创建的"mappings"下面的是"doc"，而7.9.3则是"_doc"!!需要在这里指定type为doc，否则会用_doc
+@Document(indexName = "foodie-items-ik",createIndex = false,type = "doc")
 public class Items {
     @Id
     //设置为true表示要进行存储，同时是以text类型存储，同时不进行倒排索引

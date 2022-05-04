c52d325b6c62710a7fdc1429fb741359e50bfb5a
renlingjie <309273719@qq.com>
2022-05-04 18:41:28 +0800

    # 开启上传用户头像的功能(后续可以用FastDfs)

--- domain/user/foodie-user-web/src/main/java/com/rlj/user/resource/FileUpload.java
+++ domain/user/foodie-user-web/src/main/java/com/rlj/user/resource/FileUpload.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 
 @Component//加入到容器中生效
 @ConfigurationProperties(prefix = "file")//指定前缀
-@PropertySource("classpath:file-upload-dev.properties")//指定使用类路径(resources)下的哪个配置文件
+@PropertySource("classpath:file-upload-prod.properties")//指定使用类路径(resources)下的哪个配置文件
 public class FileUpload {
     //该前缀下的所有文件名在这个类中就有值了，就是配置文件中的值
     private String imageUserFaceLocation;
